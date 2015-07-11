0b7361a0d562fbc629b1631819ec0cda7efd1bb3
azeemshaikh38 <azeem@getexp.com>
2015-07-11 00:28:05 +0000

    Updating metering

--- src/app/metering/MeteringController.js
+++ src/app/metering/MeteringController.js
@@ -10,13 +10,34 @@ angular.module('platform-ui.metering').controller(
 	[
 	'$scope',
 	'$location',
+	'$http',
 	'Title',
 	'MeteringModel',
 
 	/* Controller Definition */
-	function ($scope, $location, Title, MeteringModel) {
+	function ($scope, $location, $http, Title, MeteringModel) {
 		Title.setTitle(MeteringModel.title);
 		$scope.partnerId = $location.search()['partnerId'];
 		$scope.redirect = $location.search()['redirect'];
+		$scope.exceed = $location.search()['exceed'];
+		$http({
+			url:'http://pb.steveatgetexp.com/partners/descriptions/?partnerId='+$scope.partnerId+'&includeText=True',
+			method:'GET',
+			withCredentials:true,
+		}).success(function(data, status, headers, config) {
+			$scope.licenses=data;
+		}).error(function(data, status, headers, config) {
+		    alert("There was an error getting license information about partner. Make sure the partnerId is correct.");
+		});
+		$http({
+			url:'http://pb.steveatgetexp.com/partners/?partnerId='+$scope.partnerId,
+			method:'GET',
+			withCredentials:true,
+		}).success(function(data, status, headers, config) {
+			$scope.partner = data[0];
+		}).error(function(data, status, headers, config){
+			alert("There was an error getting partner information.");
+		});
+		$scope.license = 'def';
 	}
 ]);
