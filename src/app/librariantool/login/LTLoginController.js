fc8b2ce326eb9f964efffd15fcfe36ee901ab556
steve <steve@getexp.com>
2015-08-20 22:50:54 +0000

    PW-80: UI Development of Librarian Tool Login Page

--- src/app/librariantool/login/LTLoginController.js
+++ src/app/librariantool/login/LTLoginController.js
@@ -18,5 +18,23 @@ angular.module('platform-ui.librariantool.login').controller(
 
 	/* Controller Definition */
 	function ($scope, $http, $cookies, $location, $state, Title, LTLoginModel) {
+	    $scope.formdata = LTLoginModel.formdata;
+	    $scope.requestAccount = function() {
+		alert("requesting account NYI");
+	    }
+	    $scope.login = function() {
+		$scope.partnerId = "tair"; // should be phoenix eventually.
+                $http({
+                    url: $scope.apiUri+'/users/login/?partnerId='+$scope.partnerId,
+                    data: $scope.formdata,
+                    method: 'POST',
+                }).success(function(data, status, headers, config){
+                    $cookies.partyId = data["partyId"];
+                    $cookies.secret_key = data["secret_key"];
+                    $state.go("role.institution.iprange");
+                }).error(function(data, status, headers, config){
+                    alert('Login failed');
+                });
+	    }
 	}
 ]);
