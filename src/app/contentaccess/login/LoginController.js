4f3e79df3973b8d7e09abc0956ed4e4260dade30
andreywork <andrey@arabidopsis.org>
2015-10-27 12:30:01 -0700

    pw-123

--- src/app/contentaccess/login/LoginController.js
+++ src/app/contentaccess/login/LoginController.js
@@ -73,7 +73,7 @@ angular.module('platform-ui.contentaccess.login').controller(
 	    	//https://demoapi.arabidopsis.org/credentials/?username=andr5
 	    	 $http({
 	            	url:$scope.apiUri+'/credentials/?username='+$scope.formdata.user+'&partnerId='+$scope.partnerId,
-	            	headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+	            	//headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 	            	data: "password=2",
 	            	method:'PUT',
 	            }).success(function(data, status, headers, config){
