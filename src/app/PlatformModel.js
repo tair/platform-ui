c8bb138e612498268813ccf2dd0cd4a190f1c55b
YarikM <yarik@arabidopsis.org>
2015-11-18 10:24:53 -0800

    PW-187: Experiment 2015-11-18-10-24

--- src/app/PlatformModel.js
+++ src/app/PlatformModel.js
@@ -27,7 +27,8 @@ angular.module('platform-ui').factory(
 		brand: 'Platform UI',
 		author: 'Getexp',
 		uiDomain: '.arabidopsis.org',
-		apiUri: 'https://demoapi.arabidopsis.org',
+        apiUri: config.paywallApiBaseUri,
+        //apiUri: 'https://demoapi.arabidopsis.org',
 		//apiUri: 'http://steveapi.steveatgetexp.com',
 	    };
 	}
