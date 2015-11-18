536a14f651eb3d981aeb6a03e03fa5c699dad0df
YarikM <yarik@arabidopsis.org>
2015-11-18 02:41:09 -0800

    PW-187: Experiment 2015-11-18-02-39

--- src/app/contentaccess/subscription/individual/IndividualController.js
+++ src/app/contentaccess/subscription/individual/IndividualController.js
@@ -126,7 +126,7 @@ angular.module('platform-ui.contentaccess.subscription.individual').controller(
             $scope.info = IndividualModel.info;
             $scope.selectedSubscription = IndividualModel.selectedSubscription;
             
-            $http.get('/config/stripe.TEST.json').then(function(res) { $scope.stripePublishableKey = res.data['publishable_key']; });
+            $http.get('/config/stripe.TEST.json').then(function(res) { $scope.stripePublishableKey = res.data[0].publishable_key; });
             console.log('publishable_key=' + $scope.stripePublishableKey);
                  
         };
