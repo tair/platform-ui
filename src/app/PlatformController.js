9c9918a34ce866b247b322955911e290cedef9a9
YarikM <yarik@arabidopsis.org>
2015-11-18 10:57:38 -0800

    PW-186: Experiment 2015-11-18-10-57

--- src/app/PlatformController.js
+++ src/app/PlatformController.js
@@ -17,6 +17,8 @@ angular.module('platform-ui').controller(
 	/* Controller Definition */
 	function ($scope, $http, $cookies, $location, PlatformModel) {
 	    
+        $scope.apiUri = PlatformModel.apiUri; // PW-186: HACK: Workaround for the subsequent ASYNC HTTP request.
+
         $http.get('/config/config.json').then(function(res) { 
             config = res.data[0];
             console.log( "PW-186 DEBUG:" + config.paywallApiBaseUri);
@@ -28,7 +30,6 @@ angular.module('platform-ui').controller(
 	    $scope.brand = PlatformModel.brand;
 	    $scope.author = PlatformModel.author;
 	    $scope.menu = PlatformModel.menu;
-	    //$scope.apiUri = PlatformModel.apiUri;
 	    $scope.getRedirect = function() {
 		return encodeURIComponent($location.search()['redirect']);
 	    }
