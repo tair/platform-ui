8a6d0d337af27fcbcae9a29c64233ce3a2e7124c
YarikM <yarik@arabidopsis.org>
2015-11-18 10:40:50 -0800

    PW-186: Experiment 2015-11-18-10-40

--- src/app/PlatformController.js
+++ src/app/PlatformController.js
@@ -9,12 +9,19 @@ angular.module('platform-ui').controller(
 	/* Dependencies */
 	[
 	'$scope',
+	'$http',
 	'$cookies',
 	'$location',
 	'PlatformModel',
 
 	/* Controller Definition */
-	function ($scope, $cookies, $location, PlatformModel) {
+	function ($scope, $http, $cookies, $location, PlatformModel) {
+	    
+        $http.get('/config/config.json').then(function(res) { 
+            config = res.data[0];
+            console.log( "PW-186 DEBUG:" + config.paywallApiBaseUri)
+        });
+	    
 	    $scope.title = PlatformModel.title;
 	    $scope.brand = PlatformModel.brand;
 	    $scope.author = PlatformModel.author;
--- src/app/PlatformModel.js
+++ src/app/PlatformModel.js
@@ -14,21 +14,11 @@ angular.module('platform-ui').factory(
 	/* Controller */
 	function ($http) {
 	    
-	    console.log('PlatformModel controller called');
-
-        $http.get('/config/config.json').then(function(res) { 
-            config = res.data[0];
-            //$scope.paywallApiBaseUri = config.paywallApiBaseUri
-        });
-
-        //console.log('DEBUG2: ' + $scope.paywallApiBaseUri)
-        
 	    return {
 		title: 'PW2 Angular App',
 		brand: 'Platform UI',
 		author: 'Getexp',
 		uiDomain: '.arabidopsis.org',
-        //apiUri: config.paywallApiBaseUri,
         apiUri: 'https://demoapi.arabidopsis.org',
 		//apiUri: 'http://steveapi.steveatgetexp.com',
 	    };
