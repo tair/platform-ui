e3af93d265829dd643538cb6372cec529a8c1948
Qian Li <qianli1987@arabidopsis.org>
2015-12-10 12:24:53 -0800

    PW-81 consortium consortium page - created institution

--- src/app/librariantool/role/consortium/manage/consortium/ConsortiumManageConsortiumController.js
+++ src/app/librariantool/role/consortium/manage/consortium/ConsortiumManageConsortiumController.js
@@ -113,7 +113,7 @@ angular.module('platform-ui.librariantool.role.consortium.manage.consortium').co
                     data:data,
                     method: 'POST',
 		}).success(function(data, status, headers, config){
-			createdInstitution = data;
+			var createdInstitution = data;
 		}).error(function(data, status, headers, config){
                     alert("create new institution request failed");
 		});
