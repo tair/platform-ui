b44a5367ecbb2646607d8578dab76d8bb03b778e
Qian Li <qianli1987@arabidopsis.org>
2015-12-23 15:52:56 -0800

    PW-82 phoenix consortium page - delete affiliation fix

--- src/app/librariantool/role/phoenix/manage/institution/PhoenixManageInstitutionController.js
+++ src/app/librariantool/role/phoenix/manage/institution/PhoenixManageInstitutionController.js
@@ -222,7 +222,7 @@ angular.module('platform-ui.librariantool.role.phoenix.manage.institution').cont
 	    }
 	    $scope.deleteAffiliation = function(institution){
         	var data = {
-        			consortiumId: $cookies.consortiumId,
+        			consortiumId: $scope.consortiumId,
         			action: 'remove'
         	}
         	$http({
