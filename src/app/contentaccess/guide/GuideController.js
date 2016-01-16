a71ad8649d038324dcb1ccac7f10009d8794a235
andreywork <andrey@arabidopsis.org>
2016-01-15 20:18:42 -0800

    PW-203 getting partner by partnerId

--- src/app/contentaccess/guide/GuideController.js
+++ src/app/contentaccess/guide/GuideController.js
@@ -8,18 +8,24 @@ angular.module('platform-ui.contentaccess.guide').controller(
 
 	/* Dependencies */
 	[
-	//'$http',
+	'$http',
 	'$scope',
 	'$location',
-	//"$timeout",
+	"$timeout",
 	'Title',
 
 	/* Controller Definition */
-	function (/*$http,*/ $scope, $location, /*$timeout,*/ Title) {
+	function ($http, $scope, $location, $timeout, Title) {
 	    init();
 	    function init() {
         Title.setTitle('Troubleshooting guide');
 		$scope.partnerId = $location.search()['partnerId'];
+		$http({
+		    url:$scope.apiUri+'/partners/?partnerId='+$scope.partnerId,
+		    method:'GET',
+		}).success(function(data, status, headers, config) {
+		    $scope.partner = data[0];
+		});
 	    }
 	}
 ]);
