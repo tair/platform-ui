df38b38815e93074b0a6d383cf525c19384eb4f9
andreywork <andrey@arabidopsis.org>
2016-02-04 00:30:58 -0800

    PW-251 1.added temporarely (only for test purposes, not for released version) logging to get more info (which is an overkill, but still... I prefer to have more (even too more) info than less info). 2.commented out step 3 - initialzing subscriptions with defaul value. step 3 is not needed in any case.

--- src/app/contentaccess/subscription/individual/term/TermController.js
+++ src/app/contentaccess/subscription/individual/term/TermController.js
@@ -58,26 +58,62 @@ angular.module('platform-ui.contentaccess.subscription.individual.term').control
 	    };
 	    
 	    function init() {
+	    	var debugMsg='';
+	    	//1. inialize $scope.partnerId with $stateParams.partnerId if the $scope.partnerId is NULL
 	    	if($scope.partnerId == null){
 		    	$scope.partnerId = $stateParams.partnerId;
+		    	//vet PW-251
+		    	debugMsg='1.1 $scope.partnerId NULL. it is initialised with $stateParams.partnerId which is '+$stateParams.partnerId;
+		    	console.log(debugMsg);
+		    	//alert is not good to show for users but very useful to show for developers/debugging
+				bootbox.alert(debugMsg);//TODO comment out for release
+		    } else {
+		    	debugMsg = '1.2 $scope.partnerId is '+ $scope.partnerId+ ' and $stateParams.partnerId is'+ $stateParams.partnerId;
+		    	console.log(debugMsg);
+		    	bootbox.alert(debugMsg);//TODO comment out for release
 		    }
+	    	
+	    	//2. get partner details
 		    if($scope.partner == null){
 		    	$http({
 				    url:$scope.apiUri+'/partners/?partnerId='+$scope.partnerId,
 				    method:'GET',
 				}).success(function(data, status, headers, config) {
 				    $scope.partner = data[0];
-				});
+			    	//vet PW-251
+				    debugMsg = '2.1. OK $scope.partner is '+$scope.partner;
+			    	console.log(debugMsg);
+			    	bootbox.alert(debugMsg);
+				}).error(function(data, status, headers, config) {
+					debugMsg = '2.2. ERROR $scope.partner is '+$scope.partner;
+			    	console.log(debugMsg);
+			    	bootbox.alert(debugMsg);//TODO comment out for release
+                });
+		    } else {
+		    	debugMsg = '2.3. $scope.partner is '+$scope.partner;
+		    	console.log(debugMsg);
+		    	bootbox.alert(debugMsg);//TODO comment out for release
 		    }
-                $scope.subscriptions = TermModel.subscriptions;
-		$scope.userbool = TermModel.userbool;
-		$scope.termsbool = TermModel.termsbool;
+		    //3. initialise with default values
+		    	//PW-215 that's the place where wrong default values are assigned
+		        //commenting out the assignment
+                //$scope.subscriptions = TermModel.subscriptions;
+                $scope.userbool = TermModel.userbool;
+                $scope.termsbool = TermModel.termsbool;
+            
+            //4. rewrite the default values with correct actual values    
                 $http({
-		    url:$scope.apiUri+'/partners/terms/?partnerId='+$scope.partnerId,
-		    method:'GET',
+                	url:$scope.apiUri+'/partners/terms/?partnerId='+$scope.partnerId,
+                	method:'GET',
                 }).success(function(data, status, headers, config) {
-		    $scope.subscriptions = data;
+                	$scope.subscriptions = data; //vet PW-215 here the default values are being rewritten with correct values
+                	debugMsg = '4.1. OK $scope.subscriptions is '+$scope.subscriptions;
+    		    	console.log(debugMsg);
+    		    	bootbox.alert(debugMsg);//TODO comment out for release
                 }).error(function(data, status, headers, config) {
+                	debugMsg = '4.1. ERROR $scope.subscriptions is '+$scope.subscriptions;
+    		    	console.log(debugMsg);
+    		    	bootbox.alert(debugMsg);//TODO comment out for release
                 });
 	    };
 	}
