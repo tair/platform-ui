780b563b069c3214d5a7187bd0d851d0f5793d0e
andreywork <andrey@arabidopsis.org>
2016-06-28 15:23:32 -0700

    PW-311 ipV4 range

--- src/app/common/ipvalidator/IpValidator.js
+++ src/app/common/ipvalidator/IpValidator.js
@@ -76,29 +76,56 @@ angular.module('ipvalidator', []).service(
 			}
 			return true;
 		}
+
+	this.ip4ToNum = function(ipAddress) {
+			//A.B.C.D -> A*(256)^3 + B*(256)^2 + C*(256)^1 + D*(256)^0
+			//192.168.1.2 <-> 3232235778
+			//255.255.255.255  <->  4294967295
+			ipAddressInArray = ipAddress.split(".");
+			result = 0;
+			for (i = 0; i < ipAddressInArray.length; i++) {
+				power = 3 - i;
+				ip = parseInt(ipAddressInArray[i]);
+				result += ip * Math.pow(256, power);
+			}
+			return result;
+		}
+		
 	this.IpRangeLimit = function (start, end) {
 	    IPv4Pattern = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/;
 	    IPv6Pattern = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/;
 	    if (IPv4Pattern.test(start) && IPv4Pattern.test(end)) {
-	      length = 4;
-	      bit = 10;
-	      limit_blocks = 2;
-	      l1 = start.split('.');
-	      l2 = end.split('.');
+	      startNum = this.ip4ToNum(start);
+	      endNum = this.ip4ToNum(end);
+	      delta = endNum - startNum;
+	      if (delta > 68000000) {
+	    		return false;//invalid range. error
+	    	}
+	    	else {
+	    		return true;//valid range
+	    	}
 	    } else if (IPv6Pattern.test(start) && IPv6Pattern.test(end)) {
 	      length = 8;
 	      bit = 16;
 	      limit_blocks = 1;
-	      l1 = start.split(':');
-	      l2 = end.split(':');
-	    }
-	    for (var i = 0; i < length-limit_blocks; i++) {
-	      num1 = parseInt(l1[i], bit);
-	      num2 = parseInt(l2[i], bit);
-	      if(num1!=num2){
-	    	  return false;
+	      startArr = start.split(':');
+	      endArr = end.split(':');
+	      //Max range size for V6 ranges--CIDR /19 */ see \api-client-java\api-client-java\src\org\phoenixbioinformatics\apiclient\ip\InternetAddress.java
+	      //BigInteger MAX_V6_RANGE_SIZE = new BigInteger("324518553658426726783156020576256"); 
+	      
+	      //js can represent integers in a range of Math.pow(+-2, 53) e.g (+-)9007199254740992
+	      //see http://www.2ality.com/2012/07/large-integers.html 
+	      //we might need to use strint (“string-encoded integers”) js lib - https://github.com/rauschma/strint
+	      //or use the ip-address library [https://github.com/beaugunderson/ip-address] and its dependency jsbn [https://www.npmjs.com/package/jsbn]
+		  //leaving old code for v6 for now
+	      for (var i = 0; i < length-limit_blocks; i++) {
+		      num1 = parseInt(startArr[i], bit);
+		      num2 = parseInt(endArr[i], bit);
+		      if(num1!=num2){
+		    	  return false;//error
+		      }
 	      }
-	    }
-	    return true;
-	  };
+	      return true;
+	  }
+	} 
 	});
