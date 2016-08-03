cdd8348c303f6db539663f9613c3d0920f783377
Qian Li <qianli1987@arabidopsis.org>
2016-08-03 15:19:33 -0700

    PW-339 add subscription tab to phoenix page

---
+++ src/app/adminportal/role/phoenix/subscription/PhoenixSubscription.js
@@ -0,0 +1,19 @@
+/**
+ * Login Module
+ *
+ * The main landing page
+ */
+
+angular.module(
+    /* Name */
+    'platform-ui.adminportal.role.phoenix.subscription',
+    
+    /* Dependencies */
+    [
+	'ui.router',
+	'service.title'
+    ])
+    .config(
+	function ($stateProvider) {
+	});
+
---
+++ src/app/adminportal/role/phoenix/subscription/PhoenixSubscriptionModel.js
@@ -0,0 +1,17 @@
+/**
+ * Phoenix Subscription Model
+ */
+
+angular.module('platform-ui.adminportal.role.phoenix.subscription').factory(
+	/* Name */
+	'PhoenixSubscriptionModel',
+
+	/* Dependencies */
+	[
+
+	function () {
+		return {
+			currentTab: {label:"SUBSCRIPTION", state:"role.phoenix.subscription"},
+		};
+	}
+]);
