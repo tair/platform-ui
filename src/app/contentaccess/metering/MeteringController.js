ca1d47c3387343044f7d27d0d2925cf715939955
Qian Li <qianli1987@arabidopsis.org>
2016-09-23 11:34:37 -0700

    PW-377 get partner info fix

--- src/app/contentaccess/metering/MeteringController.js
+++ src/app/contentaccess/metering/MeteringController.js
@@ -17,17 +17,9 @@ angular.module('platform-ui.contentaccess.metering').controller(
 
 	/* Controller Definition */
 	function ($scope, $location, $http, $cookies, Title, MeteringModel) {
+		init();
 		Title.setTitle(MeteringModel.title);
 		$scope.partnerId = $location.search()['partnerId'];
-		$scope.partner = null;
-		$http({
-			url:$scope.apiUri+'/partners/?partnerId='+$scope.partnerId,
-			method:'GET',
-		}).success(function(data, status, headers, config) {
-			$scope.partner = data[0];
-		}).error(function(data, status, headers, config){
-			alert("There was an error getting partner information.");
-		});
 		$scope.redirect = $scope.partner['defaultLoginRedirect'];
 	    $scope.redirectNoEncode = decodeURIComponent($scope.partner['defaultLoginRedirect']);
 		$scope.exceed = $location.search()['exceed'];
@@ -40,5 +32,15 @@ angular.module('platform-ui.contentaccess.metering').controller(
 		    alert("There was an error getting license information about partner. Make sure the partnerId is correct.");
 		});
 		$scope.license = 'def';
+		function init(){
+			$http({
+				url:$scope.apiUri+'/partners/?partnerId='+$scope.partnerId,
+				method:'GET',
+			}).success(function(data, status, headers, config) {
+				$scope.partner = data[0];
+			}).error(function(data, status, headers, config){
+				alert("There was an error getting partner information.");
+			});
+		}
 	}
 ]);
