a52c07da11d3838c2800a4d0be5a87b67da9b471
Qian Li <qianli1987@arabidopsis.org>
2016-10-23 19:37:04 -0700

    PW-391 forgotUsername function impl

--- src/app/adminportal/login/forgotusername/ForgotUsernameController.js
+++ src/app/adminportal/login/forgotusername/ForgotUsernameController.js
@@ -18,25 +18,20 @@ angular.module('platform-ui.adminportal.login.forgotusername').controller(
 	/* Controller Definition */
 	function ($scope, $http, $cookies, $location, $state, Title, ForgotUsernameModel) {
 		$scope.forgotUsername = function() {
-//			if($scope.email != null) {
-//				data={
-//						email:$scope.email,
-//						partnerId: 'phoenix',
-//				}
-//				$http({
-//                    url: $scope.apiUri+'/credentials/resetPwd/',
-//                    data: data,
-//                    method: 'POST',
-//                }).success(function(data, status, headers, config){
-//                	$state.go('ltlogin.forgotpassword.thankyou', {'username':$scope.username});
-//                }).error(function(data, status, headers, config){
-//                	if(data['reset pwd failed'] == 'No such user'){
-//                		$scope.invalidUsernameMsg = true;
-//                	}
-//                });				
-//			}else{
-//				alert("Please enter your eamil!")
-//			}
+			if($scope.email != null) {
+				$http({
+                    url: $scope.apiUri+'/credentials/?email='+$scope.email,
+                    method: 'GET',
+                }).success(function(data, status, headers, config){
+                		$state.go('ltlogin.forgotpassword.thankyou', {'email':$scope.email});
+                }).error(function(data, status, headers, config){
+                	if (data['error'] == 'no username found.'){
+                		$scope.invalidEmailMsg = true;
+                	}
+                });				
+			}else{
+				alert("Please enter your eamil!")
+			}
 		}
 		$scope.back = function(){
 			$state.go('ltlogin.forgotpassword.sendlink');
