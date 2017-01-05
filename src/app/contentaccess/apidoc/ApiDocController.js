805bc893cb7654d1edb60a1a079aa8c400c82ee1
bob-muller <muller@phoenixbioinformatics.org>
2017-01-04 17:45:48 -0800

    PW-435: correct another copy-paste error in controller

--- src/app/contentaccess/apidoc/ApiDocController.js
+++ src/app/contentaccess/apidoc/ApiDocController.js
@@ -3,38 +3,34 @@
  */
 
 angular.module('platform-ui.contentaccess.apidoc').controller(
-	/* Name */
-	'ApiDocController',
+/* Name */
+'ApiDocController',
 
-	/* Dependencies */
-	[
-	'$http',
-	'$scope',
-	'$location',
-	'$state',
-	'Title',
-	'ApiDocModel',
+/* Dependencies */
+[ '$http', '$scope', '$location', '$state', 'Title', 'ApiDocModel',
 
-	/* Controller Definition */
-	function ($http, $scope, $location, $state, Title, SubscriptionModel) {
-	    init();
-	    console.log($state);
+/* Controller Definition */
+function($http, $scope, $location, $state, Title, ApiDocModel) {
+	init();
+	console.log($state);
 
-            $scope.switchTab = function(tabName) {
-		//$scope.currentTab = tabName;
-		if (tabName=="landing") {
-			$state.go("apidoc.landing", {partnerId:$scope.partnerId,redirect:$scope.redirect});
+	$scope.switchTab = function(tabName) {
+		// $scope.currentTab = tabName;
+		if (tabName == "landing") {
+			$state.go("apidoc.landing", {
+				partnerId : $scope.partnerId,
+				redirect : $scope.redirect
+			});
 			console.log($state);
 			return;
 		}
-	    };
+	};
 
-	    function init() {
-                Title.setTitle(ApiDocModel.title);//PW-264
-                $scope.initialheading = ApiDocModel.initialheading;
-                $scope.currentTab = ApiDocModel.currentTab;
-                $scope.tabs = ApiDocModel.tabs;
+	function init() {
+		Title.setTitle(ApiDocModel.title);// PW-264
+		$scope.initialheading = ApiDocModel.initialheading;
+		$scope.currentTab = ApiDocModel.currentTab;
+		$scope.tabs = ApiDocModel.tabs;
 		$scope.templates = ApiDocModel.templates;
-	    }
 	}
-]);
+} ]);
