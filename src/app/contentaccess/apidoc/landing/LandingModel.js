2fe6d30ee1546fff1d07b8718d1813845beaedc5
bob-muller <muller@phoenixbioinformatics.org>
2017-01-05 13:21:28 -0800

    PW-684: Replace encoded <> with {} for variable

--- src/app/contentaccess/apidoc/landing/LandingModel.js
+++ src/app/contentaccess/apidoc/landing/LandingModel.js
@@ -41,7 +41,7 @@ angular
 								elements : [
 										'The Subscription Management System (SMS) REST API gives you a complete system for subscription management.',
 										'SMS resources include partners, parties, credentials, subscriptions, meters, authorizations, and session logs.',
-										'The API calls are Uniform Resource Identifiers (URIs) that identify resources or actions. You can use the GET, POST, PUT, and DELETE HTTP operations to query, create, update, or delete the resources, respectively. The substitution value &lt;id&gt; is a unique integer identifier for an object, used throughout the documentation.',
+										'The API calls are Uniform Resource Identifiers (URIs) that identify resources or actions. You can use the GET, POST, PUT, and DELETE HTTP operations to query, create, update, or delete the resources, respectively. The substitution value {id} is a unique integer identifier for an object, used throughout the documentation.',
 										'You submit new and updated data values in the body of the POST or PUT operations in form-data or form-urlencoded format.',
 										'Most API calls require authentication. See the Credentials API for more information. You must be authenticated as a registered user through one of the partner registration systems.'
 										]
