e90c4611b51b634e69f34fa4bef86329fec80c90
bob-muller <muller@phoenixbioinformatics.org>
2017-01-05 10:25:58 -0800

    Remove sub-modules not yet installed

--- src/app/contentaccess/apidoc/ApiDocModel.js
+++ src/app/contentaccess/apidoc/ApiDocModel.js
@@ -19,14 +19,14 @@ angular.module('platform-ui.contentaccess.apidoc').factory(
 		templates : {
 		    landing: 'contentaccess/apidoc/landing/landing.html',
 		    partners: 'contentaccess/apidoc/partners/partners.html',
-		    parties: 'contentaccess/apidoc/parties/parties.html',
+		    /*parties: 'contentaccess/apidoc/parties/parties.html',
 		    ipranges: 'contentaccess/apidoc/ipranges/ipranges.html',
 		    subscriptions: 'contentaccess/apidoc/subscriptions/subscriptions.html',
 		    meters: 'contentaccess/apidoc/meters/meters.html',
 		    payments: 'contentaccess/apidoc/payments/payments.html',
 		    credentials: 'contentaccess/apidoc/credentials/credentials.html',
 		    authorizations: 'contentaccess/apidoc/authorizations/authorizations.html',
-		    sessionlogs: 'contentaccess/apidoc/sessionlogs/sessionlogs.html',
+		    sessionlogs: 'contentaccess/apidoc/sessionlogs/sessionlogs.html',*/
 		},
 	    }
 	}
