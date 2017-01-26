70a07db606238ff874ab71930a4b1273fec80c10
Qian Li <qianli1987@arabidopsis.org>
2017-01-25 18:12:32 -0800

    PW-267 subscriptionrequest file type csv

--- src/app/adminportal/role/phoenix/subscription/PhoenixSubscriptionController.js
+++ src/app/adminportal/role/phoenix/subscription/PhoenixSubscriptionController.js
@@ -24,15 +24,14 @@ angular.module('platform-ui.adminportal.role.phoenix.subscription').controller(
 	    init();
 	    
 	    $scope.downloadRequest = function(){
-	    		//$window.location.href = $scope.apiUri+'/subscriptions/subscriptionrequest/?credentialId='+$scope.credentialId+'&secretKey='+encodeURIComponent($scope.secretKey);
-	    	$http({
+	    		$http({
 				url: $scope.apiUri+'/subscriptions/subscriptionrequest/?credentialId='+$scope.credentialId+'&secretKey='+encodeURIComponent($scope.secretKey),
 				method: 'GET',
 				headers: {'Authorization': 'JWT '+$scope.token},
 			    }).success(function(data, status, headers, config){
 			    	//http://alferov.github.io/angular-file-saver/
-			    	var data = new Blob([data], { type: 'text/plain;charset=utf-8' });
-			        FileSaver.saveAs(data, 'report.txt');
+			    	var data = new Blob([data], { type: 'text/csv;charset=utf-8' });
+			        FileSaver.saveAs(data, 'report.csv');
 			    	/*If you need to save really large files bigger then the blob's size limitation or don't 
 			    	have enough RAM, then have a look at the more advanced StreamSaver.js.*/
 			    	console.log('downloadRequest success.');
@@ -40,8 +39,7 @@ angular.module('platform-ui.adminportal.role.phoenix.subscription').controller(
 			    	console.log('downloadRequest error.');
 			    });
 	    }
-	    $scope.downloadLink = $scope.apiUri+'/subscriptions/subscriptionrequest/?credentialId='+$scope.credentialId+'&secretKey='+encodeURIComponent($scope.secretKey);
-	    	function init() {
+	    function init() {
 	    		$scope.setCurrentTab(PhoenixProfileModel.currentTab);
 //				if(!$scope.credentialId || !$scope.secretKey){
 //					$state.go('ltlogin');
