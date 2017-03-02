80d8bf496296f3b723d3678ea61670da819da333
Qian Li <qianli1987@arabidopsis.org>
2017-03-02 14:54:37 -0800

    PW-81 name field issue fix

--- src/app/adminportal/role/consortium/usage/ConsortiumUsageController.js
+++ src/app/adminportal/role/consortium/usage/ConsortiumUsageController.js
@@ -58,8 +58,8 @@ angular.module('platform-ui.adminportal.role.consortium.usage').controller(
 			}).success(function(data, status, headers, config) {
 				$scope.postData.consortium = data[0].name;//TODO: partyType, partyName as parameter
 				$scope.postData.email = data[1].email;
-				$scope.postData.name = (data[1].firstName != null)?data[1].firstName:"" 
-					+ " " + (data[1].lastName != null)?data[1].lastName:"";
+				$scope.postData.name = ((data[1].firstName != null)?data[1].firstName:"") 
+					+ " " + ((data[1].lastName != null)?data[1].lastName:"");
 				
 				
 			}).error(function() {
