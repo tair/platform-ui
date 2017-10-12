ab28fe5fe2d95edac00f2374c0d5d37bd3d2755e
ChenXingguo <xingguochen@alumni.stanford.edu>
2017-10-12 15:23:25 -0700

    TAIR-2974: Add partner id for API calls to get username by email address.

--- src/app/contentaccess/login/forgetusername/ForgetUsernameController.js
+++ src/app/contentaccess/login/forgetusername/ForgetUsernameController.js
@@ -22,8 +22,9 @@ angular.module('platform-ui.contentaccess.login.forgetusername').controller(
 		$scope.sendUsernames = function() {
 			if($scope.email != null) {
 				var email = $scope.email.replace('+', '%2B');
+				$scope.partnerId = 'tair';
 				$http({
-                    url: $scope.apiUri+'/credentials/getUsernames/?email='+email,
+                    url: $scope.apiUri+'/credentials/getUsernames/?email='+email+'&partnerId='+$scope.partnerId,
                     method: 'GET',
                 }).success(function(data, status, headers, config){
                 	$state.go('login.forgetusername.thankyou', {'email':$scope.email});
