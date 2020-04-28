911a97be24a1bb4935aa7cf832b3cea3d0d58655
ChenXingguo <xingguochen@alumni.stanford.edu>
2020-04-28 13:27:44 -0700

    MBANK-31: add js to save data.

--- src/app/adminportal/role/institution/banner/InstitutionMembershipBannerController.js
+++ src/app/adminportal/role/institution/banner/InstitutionMembershipBannerController.js
@@ -31,10 +31,36 @@ angular.module('platform-ui.adminportal.role.institution.banner').controller(
                 put_data = {}
                 //put original values from GET
                 put_data["partyId"]  = $scope.institutionId;
-                put_data["imageUrl"]= $scope.imageInfo.imageUrl;
                 put_data["name"]= $scope.imageInfo.name;
+                put_data["imageUrl"]= $scope.imageInfo.imageUrl;
+
+                if ($scope.isNew) {
+                    $http({
+                        url: $scope.apiUri+'/parties/imageinfo/?credentialId='+$scope.credentialId+'&secretKey='+encodeURIComponent($scope.secretKey),
+                        data: put_data,
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    }).success(function(data, status, headers, config){
+                        $scope.edit = false;
+                        $scope.isNew = false;
+                    }).error(function(data, status, headers, config) {
+                        bootbox.alert("Failed to create banner info "+data['error']);
+                        $scope.cancel();
+                    });
+                } else {
+                    $http({
+                        url: $scope.apiUri+'/parties/imageinfo/?partyId='+$scope.institutionId+'&credentialId='+$scope.credentialId+'&secretKey='+encodeURIComponent($scope.secretKey),
+                        data: put_data,
+                        method: 'PUT',
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    }).success(function(data, status, headers, config){
+                        $scope.edit = false;
+                    }).error(function(data, status, headers, config) {
+                        bootbox.alert("Failed to update banner info "+data['error']);
+                        $scope.cancel();
+                    });
+                }
             }
-            $scope.edit = !$scope.edit;
         }
 
         $scope.cancel = function() {
@@ -42,7 +68,7 @@ angular.module('platform-ui.adminportal.role.institution.banner').controller(
         }
 
         function validateInfo() {
-            return true;
+            return $scope.imageInfo.name && $scope.imageInfo.imageUrl;
         }
 
         function init() {
@@ -51,16 +77,21 @@ angular.module('platform-ui.adminportal.role.institution.banner').controller(
             $scope.uiparams = InstitutionMembershipBannerModel.uiparams;
             $scope.imageInfo.partyId = $scope.institutionId;
             $scope.edit = true;
+            $scope.isNew = true;
             $http({
                 url: $scope.apiUri+'/parties/imageinfo/?partyId='+$scope.institutionId+'&credentialId='+$scope.credentialId+'&secretKey='+encodeURIComponent($scope.secretKey),
                 method: 'GET',
             }).success(function(data, status, headers, config){
-                $scope.imageInfo.name = data[0].name;
-                $scope.imageInfo.imageUrl = data[0].imageUrl;
-                $scope.edit = false;
+                if (data.length > 0) {
+                    $scope.imageInfo.name = data[0].name;
+                    $scope.imageInfo.imageUrl = data[0].imageUrl;
+                    $scope.edit = false;
+                    $scope.isNew = false;
+                }
             }).error(function(data, status, headers, config){
-                // errMsg = "GET /parties/imageinfo/ Failed";
-                // bootbox.alert(errMsg);
+                $("#banner-info").hide();
+                $("#error-msg").show();
+                $("#error-msg-text").html("Cannot load banner information. Please refresh the page or try again later.");
             });
         }
     }
