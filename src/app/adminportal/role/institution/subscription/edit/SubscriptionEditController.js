00d557e6c9f18cea0b06e60145a49553c2e3551c
ChenXingguo <xingguochen@alumni.stanford.edu>
2020-06-10 15:16:13 -0700

    Check banner info for morphobank subscription.

--- src/app/adminportal/role/institution/subscription/edit/SubscriptionEditController.js
+++ src/app/adminportal/role/institution/subscription/edit/SubscriptionEditController.js
@@ -68,32 +68,50 @@ angular.module('platform-ui.adminportal.role.institution.subscription.edit').con
 	    };
 
 	    function init() {
-		$scope.uiparams = SubscriptionEditModel.uiparams;
-		$scope.partnerId = $location.search()['partnerId'];
-//		if(!$scope.credentialId || !$scope.secretKey){
-//			$state.go('ltlogin');
-//		}
-		if ($scope.partnerId in $scope.allSubscriptions){
-			$scope.transactionType = "renew";
-			if ($scope.allSubscriptions[$scope.partnerId].startDate){
-				$scope.startDate = $scope.allSubscriptions[$scope.partnerId].startDate.split('T')[0];
+			$scope.uiparams = SubscriptionEditModel.uiparams;
+			$scope.partnerId = $location.search()['partnerId'];
+	//		if(!$scope.credentialId || !$scope.secretKey){
+	//			$state.go('ltlogin');
+	//		}
+			if ($scope.partnerId in $scope.allSubscriptions){
+				$scope.transactionType = "renew";
+				if ($scope.allSubscriptions[$scope.partnerId].startDate){
+					$scope.startDate = $scope.allSubscriptions[$scope.partnerId].startDate.split('T')[0];
+				}
+				if ($scope.allSubscriptions[$scope.partnerId].endDate){
+					$scope.endDate = $scope.allSubscriptions[$scope.partnerId].endDate.split('T')[0];
+				}
+			}else{
+				$scope.transactionType = "create";
 			}
-			if ($scope.allSubscriptions[$scope.partnerId].endDate){
-				$scope.endDate = $scope.allSubscriptions[$scope.partnerId].endDate.split('T')[0];
-			}
-		}else{
-			$scope.transactionType = "create";
-		}
-		$(function () {
-            $('#startDate').datepicker({
-        		dateFormat: "yy-mm-dd"
-            });
-        });
-		$(function () {
-            $('#endDate').datepicker({
-        		dateFormat: "yy-mm-dd"
-            });
-        });
-	    }
+			$(function () {
+	            $('#startDate').datepicker({
+	        		dateFormat: "yy-mm-dd"
+	            });
+	        });
+			$(function () {
+	            $('#endDate').datepicker({
+	        		dateFormat: "yy-mm-dd"
+	            });
+	        });
+	        if ($scope.partnerId == "morphobank") {
+	        	// check banner image info
+	        	$http({
+	                url: $scope.apiUri+'/parties/imageinfo/?partyId='+$scope.institutionId+'&credentialId='+$scope.credentialId+'&secretKey='+encodeURIComponent($scope.secretKey),
+	                method: 'GET',
+	            }).success(function(data, status, headers, config){
+	                if (data.length > 0) {
+	                    // do nothing
+	                } else {
+	                    $("#warning-msg").show();
+	                	$("#warning-msg-text").html("Please set up banner before adding subscription");
+	                }
+	            }).error(function(data, status, headers, config){
+	                $("#warning-msg").show();
+	                $("#warning-msg-text").html("Please set up banner before adding subscription");
+	            });
+		    }
+        }
+        
 	}
 ]);
